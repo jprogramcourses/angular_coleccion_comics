f8d6870f715dfbc656e34157a3b7ee54630749a4
jprogramcourses <jprogramcourses@gmail.com>
2021-01-24 18:36:51 +0100

    Modificación del servicio de colecciones para obtener los datos a partir del servicio rest creado con Spring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,12 @@ import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ColeccionesComponent } from './colecciones/colecciones.component';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/clientes', pathMatch: 'full'},
-  {path: 'directivas', component: DirectivaComponent},
-  {path: 'colecciones', component: ColeccionesComponent},
+  { path: '', redirectTo: '/colecciones', pathMatch: 'full' },
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'colecciones', component: ColeccionesComponent },
 ]
 
 @NgModule({
@@ -24,6 +25,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/colecciones/coleccion.service.ts
+++ src/app/colecciones/coleccion.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { COLECCION } from './colecciones.json';
 import { Coleccion } from './coleccion';
-import { Observable , of} from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColeccionService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8080/apicolec/colecciones';
 
-  getColecciones() : Observable<Coleccion[]>{
-    return of(COLECCION);
+  constructor(private http: HttpClient) { }
+
+  getColecciones(): Observable<Coleccion[]> {
+    // return of(COLECCION);
+    // Primera forma para castear los resultados recibidos del servicio rest.
+    // El resultado recibido se castea con get<Coleccion[]>:
+    // return this.http.get<Coleccion[]>(this.urlEndPoint);
+    // Segunda forma de castear los resultados, a través de un map
+    // y una función lambda:
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Coleccion[])
+    );
   }
 }
